e51f3d5fc58ba5c43fa157e2342b8048c6127346
Adan <asanchez00@unisalle.edu.co>
2026-03-26 10:44:34 -0500

    Creación de empresa de transporte de acuerdo a UML

---
+++ src/model/EmpresaTransporte.java
@@ -0,0 +1,22 @@
+package model;
+
+public abstract class EmpresaTransporte {
+    private String nombre;
+    public EmpresaTransporte(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Cada empresa implementa su propia lógica de entrega.
+     */
+    public abstract void entregarPedido(OrdenDeCompra orden);
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
